02aa31b8ef11c88fdc9e3feb33b7f1ca4a2be044
Lucas Hansen <lucas.hansenst@gmail.com>
2023-11-26 00:31:13 -0600

    Updated onscreen data and created cards

--- assets/script.js
+++ assets/script.js
@@ -1,47 +1,109 @@
 var searchButton = document.getElementById("searchButton");
 var searchHistory = document.getElementById("searchHistory");
 var cityName = document.querySelector(".cityName");
+var iconSpan = document.querySelector(".iconSpan");
+var currentDate = document.querySelector(".currentDate");
 var temp = document.querySelector('.temp');
-var wind = document.querySelector('wind');
-var humidty = document.querySelector('humidty');
+var wind = document.querySelector('.wind');
+var humidity = document.querySelector('.humidity');
 var cardContainer = document.getElementById("fiveDayCards");
 var cityToSearch = document.getElementById('searchForm');
 var apiKey = "53a5d7cd95056d4670ee8a21a7e50e28";
 var lat;
 var lon;
 
-function searchHandler(event) {
+function getCoordinates() {
     let value = cityToSearch.value.trim();
-    getCoordinates(value);
-    console.log(lat, lon);
 
-    fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`)
-    .then(response => {
-        if(!response.ok) {
-            throw new Error("Failed HTTP code " + response.status);
-        }
-        return response;
-    })
+    // call api to find latitude and longitude of user search  
+    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${value}&limit=1&appid=${apiKey}`)
     .then((response) => response.json())
     .then((data) => {
-        console.log(data);
-        
+
+        if (data) {
+            lat = data[0].lat;
+            lon = data[0].lon;
+            searchHandler();
+        }
     })
     .catch(err => {
         console.error(err);
     })
 }
 
-function getCoordinates(city) {
-    let value = city;
+function searchHandler() {
+        // call both APIs for current weather and 5-day forecast using below lat and lon values
+        Promise.all([
+            fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`),
+            fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`)
+        ])
+            .then(responses => Promise.all(responses.map(response => response.json())))
+            .then((data) => {
+                console.log(data);
+                
+                
+                let unixDate = data[1].dt;
+                let date = new Date(unixDate * 1000);
 
-    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${value}&limit=1&appid=${apiKey}`)
-    .then((response) => response.json())
-    .then((data) => {
+                // Updating onscreen values for currentCity
+                let hiddenValues = document.querySelectorAll(".hidden");
+                for (let i = 0; i < hiddenValues.length; i++) {
+                    hiddenValues[i].style.visibility = "visible";
+                }
 
-        lat = data[0].lat.toFixed(2);
-        lon = data[0].lon.toFixed(2);
-    });
+                currentDate.textContent = date;
+                cityName.textContent = data[1].name;
+                iconSpan.innerHTML = `
+                        <span class="currentCityImage">
+                            <img src="https://openweathermap.org/img/wn/${data[1].weather[0].icon}@2x.png"><img>
+                        </span>
+                    `
+                temp.textContent = data[1].main.temp;
+                wind.textContent = data[1].wind.speed;
+                humidity.textContent = data[1].main.humidity;
+
+                // create 5-day forecast cards and append onscreen
+                createCards(data);
+
+            })
+            .catch(err => {
+                console.error(err);
+            });
 }
 
-searchButton.addEventListener("click", searchHandler);
+function createCards(data) {
+    var index = 2;
+
+    for (let i = 0; i < 5; i++) {
+
+        let unixDate = data[0].list[index].dt;
+        let date = new Date(unixDate * 1000);
+
+
+        const forecastCard = document.createElement("div");
+        forecastCard.setAttribute("class", "card col border border-primary");
+        forecastCard.innerHTML = `
+            <div class="card-header">${date.toLocaleDateString("en-US")} 
+                
+            </div>
+            <div class="card-body">
+                <span class="cardImage">
+                    <img src="https://openweathermap.org/img/wn/${data[0].list[index].weather[0].icon}@2x.png"><img>
+                </span>
+
+                <p>Temp: ${data[0].list[index].main.temp}°F</p>
+
+                <p>Wind: ${data[0].list[index].wind.speed} MPH</p>
+
+                <p>Humidity: ${data[0].list[index].main.humidity}%</p>
+            </div>
+        `;
+
+        cardContainer.append(forecastCard);
+        index += 8;
+    }
+}
+
+
+
+searchButton.addEventListener("click", getCoordinates);
